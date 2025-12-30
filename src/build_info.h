2266474f8c254b7cf47fe15dc5fed0c54b6c940c
neledov <neledov@gmail.com>
2025-12-30 13:51:07 +0100

    pig smart now. pig knows you lora or not. tx/tx/xo

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2025-12-27T14:46:48.025685"
+#define BUILD_TIME "2025-12-30T13:36:14.640557"
 #define BUILD_VERSION "0.1.7_DNKROZ"
-#define BUILD_COMMIT "7f7ff6a"
+#define BUILD_COMMIT "fe27853"

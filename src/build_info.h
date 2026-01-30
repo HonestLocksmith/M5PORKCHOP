3586da9a2e166f1edb2776070412dbcff28d9ba4
neledov <neledov@gmail.com>
2026-01-30 21:17:22 +0100

    THE HEAP. THE HEAT. THE FIGHT.

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2026-01-30T17:45:36.107063"
+#define BUILD_TIME "2026-01-30T21:11:19.800835"
 #define BUILD_VERSION "0.1.8-heapkampf"
-#define BUILD_COMMIT "7ad8e01"
+#define BUILD_COMMIT "6337c46"
